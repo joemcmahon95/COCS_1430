5c300a9677fd9f44be892034da05ac3e63260248
SyedMSRaza <smshabbirraza@gmail.com>
2017-04-05 17:03:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,10 @@ using namespace std;
 
 int main(){
   
+int nameScore =0;  
+for (int i=0;i<line.length();i++)
+{
+  char letter = line[i];
+  nameScore += (int) letter - 64;
+}
+totalscore += nameScore;
